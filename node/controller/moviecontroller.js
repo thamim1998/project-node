fc853027c71e4c99974b67f364a6eb9d9699e0b5
Thamimul Ansari <thamim1997@gmail.com>
2023-01-20 00:18:52 +0100

    sort based popularity

--- node/controller/moviecontroller.js
+++ node/controller/moviecontroller.js
@@ -3,14 +3,25 @@ const axios = require("axios");
 const url =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=eee8f2920fa28653902a137dcad5b867";
 
+const sortMovies = (data) => {
+  
+};
+
 //get all movies
-const getMovies = async (req,rev) => {
+const getMovies = async (req, res) => {
   try {
-    const res = await axios.get(url);
-    rev.send(res.data);
-    return res.data
-  } 
-  catch (error) {
+    const rev = await axios.get(url);
+
+    const movie = rev.data.results;
+
+    //for sorting movies based on popularity
+    const new_movie = movie.sort((a, b) => {
+      a.popularity - b.popularity;
+    });
+
+    res.send(new_movie);
+    
+  } catch (error) {
     console.error(error);
   }
 };
